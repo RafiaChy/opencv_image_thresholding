42bdfe1531bd1462a26d3209558d49fb728b8522
RafiaChy <rafiachowdhury24@gmail.com>
2022-01-07 23:31:18 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+image = cv2.imread('gradient.png', 1)
+
+_, thresh_binary = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+_, thresh_binary_inverse = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)
+_, thresh_trunc = cv2.threshold(image, 79, 255, cv2.THRESH_TRUNC)
+_, thresh_tozero = cv2.threshold(image, 79, 255, cv2.THRESH_TOZERO)
+_, thresh_tozero_inv = cv2.threshold(image, 79, 255, cv2.THRESH_TOZERO_INV)
+cv2.imshow('image', image)
+cv2.imshow('thresh_binary', thresh_binary)
+cv2.imshow('thresh_binary_inverse', thresh_binary_inverse)
+cv2.imshow('thresh_trunc', thresh_trunc)
+cv2.imshow('thresh_tozero', thresh_tozero)
+cv2.imshow('thresh_tozero_inv', thresh_tozero_inv)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
